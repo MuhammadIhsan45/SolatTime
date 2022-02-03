4eb037c927b3c687558390c02ac99d12fb0c58bf
MuhammadIhsan45 <muhsanz41@gmail.com>
2022-02-04 00:12:14 +0700

    SolatTIme

---
+++ 20200140034_UTS_PAT/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace _20200140034_UTS_PAT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.WaktuINI = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SubuhLabel = new System.Windows.Forms.Label();
+            this.DuhurLabel = new System.Windows.Forms.Label();
+            this.AsarLabel = new System.Windows.Forms.Label();
+            this.MaghribLabel = new System.Windows.Forms.Label();
+            this.IsyaLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(210, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WAKTU SOLAT YOGYAKARTA";
+            // 
+            // WaktuINI
+            // 
+            this.WaktuINI.AutoSize = true;
+            this.WaktuINI.Location = new System.Drawing.Point(358, 118);
+            this.WaktuINI.Name = "WaktuINI";
+            this.WaktuINI.Size = new System.Drawing.Size(24, 17);
+            this.WaktuINI.TabIndex = 1;
+            this.WaktuINI.Text = "__";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SUBUH";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 341);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ISYA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "MAGHRIB";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "DUHUR";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(126, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ASAR";
+            // 
+            // SubuhLabel
+            // 
+            this.SubuhLabel.AutoSize = true;
+            this.SubuhLabel.Location = new System.Drawing.Point(278, 181);
+            this.SubuhLabel.Name = "SubuhLabel";
+            this.SubuhLabel.Size = new System.Drawing.Size(18, 17);
+            this.SubuhLabel.TabIndex = 7;
+            this.SubuhLabel.Text = "--";
+            // 
+            // DuhurLabel
+            // 
+            this.DuhurLabel.AutoSize = true;
+            this.DuhurLabel.Location = new System.Drawing.Point(278, 221);
+            this.DuhurLabel.Name = "DuhurLabel";
+            this.DuhurLabel.Size = new System.Drawing.Size(18, 17);
+            this.DuhurLabel.TabIndex = 8;
+            this.DuhurLabel.Text = "--";
+            // 
+            // AsarLabel
+            // 
+            this.AsarLabel.AutoSize = true;
+            this.AsarLabel.Location = new System.Drawing.Point(278, 261);
+            this.AsarLabel.Name = "AsarLabel";
+            this.AsarLabel.Size = new System.Drawing.Size(18, 17);
+            this.AsarLabel.TabIndex = 9;
+            this.AsarLabel.Text = "--";
+            // 
+            // MaghribLabel
+            // 
+            this.MaghribLabel.AutoSize = true;
+            this.MaghribLabel.Location = new System.Drawing.Point(278, 301);
+            this.MaghribLabel.Name = "MaghribLabel";
+            this.MaghribLabel.Size = new System.Drawing.Size(18, 17);
+            this.MaghribLabel.TabIndex = 10;
+            this.MaghribLabel.Text = "--";
+            // 
+            // IsyaLabel
+            // 
+            this.IsyaLabel.AutoSize = true;
+            this.IsyaLabel.Location = new System.Drawing.Point(278, 341);
+            this.IsyaLabel.Name = "IsyaLabel";
+            this.IsyaLabel.Size = new System.Drawing.Size(18, 17);
+            this.IsyaLabel.TabIndex = 11;
+            this.IsyaLabel.Text = "--";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 120);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Get Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.IsyaLabel);
+            this.Controls.Add(this.MaghribLabel);
+            this.Controls.Add(this.AsarLabel);
+            this.Controls.Add(this.DuhurLabel);
+            this.Controls.Add(this.SubuhLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WaktuINI);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label WaktuINI;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label SubuhLabel;
+        private System.Windows.Forms.Label DuhurLabel;
+        private System.Windows.Forms.Label AsarLabel;
+        private System.Windows.Forms.Label MaghribLabel;
+        private System.Windows.Forms.Label IsyaLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 20200140034_UTS_PAT/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20200140034_UTS_PAT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            string endpoint = "http://api.aladhan.com/v1/calendarByCity";
+            string parameters = "?country=Indonesia&city=Yogyakarta";
+            string uri = endpoint + parameters;
+            HttpResponseMessage responeMessage = httpClient.GetAsync(uri).Result;
+            string response = responeMessage.Content.ReadAsStringAsync().Result;
+            dynamic resultObject = JsonConvert.DeserializeObject(response);
+
+            WaktuINI.Text = DateTime.Now.ToString();
+
+            SubuhLabel.Text = resultObject.data[0].timings.Fajr;
+            DuhurLabel.Text = resultObject.data[0].timings.Dhuhr;
+            AsarLabel.Text = resultObject.data[0].timings.Asr;
+            MaghribLabel.Text = resultObject.data[0].timings.Maghrib;
+            IsyaLabel.Text = resultObject.data[0].timings.Isha;
+
+        }
+    }
+}
